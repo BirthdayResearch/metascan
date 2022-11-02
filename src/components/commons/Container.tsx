3d65e3b5015f5260a22621bd0851c1152504d0b8
Harsh <harshrathi.dev@gmail.com>
2022-11-02 12:58:46 +0530

    removed classname package

--- src/components/commons/Container.tsx
+++ src/components/commons/Container.tsx
@@ -1,5 +1,5 @@
 import { PropsWithChildren } from "react";
-import classNames from "classnames";
+import clsx from "clsx";
 
 /**
  * Container implementation that is part of the design language.
@@ -8,7 +8,5 @@ export default function Container({
   className,
   children,
 }: PropsWithChildren<{ className?: string }>): JSX.Element {
-  return (
-    <div className={classNames("container mx-auto", className)}>{children}</div>
-  );
+  return <div className={clsx("container mx-auto", className)}>{children}</div>;
 }

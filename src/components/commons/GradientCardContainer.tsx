dd679430dd48aac0902916679c18e6eb3c83c451
veralygit <57183851+veralygit@users.noreply.github.com>
2022-11-04 09:41:38 +0800

    remove logging in code

--- src/components/commons/GradientCardContainer.tsx
+++ src/components/commons/GradientCardContainer.tsx
@@ -5,7 +5,6 @@ interface GradientCardProps {
 }
 
 function GradientCardContainer({ children }: GradientCardProps) {
-  console.log(children);
   return (
     <div className="rounded-[15px] w-full h-full p-[0.5px] white-gradient-1 drop-shadow-[0_5px_20px_rgba(0,0,0,0.2)]">
       <div className="bg-black-900 black-gradient-1 rounded-[15px] w-full h-full">

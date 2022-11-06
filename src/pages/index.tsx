af1a22a454f0bd0670170b1a20ef0d65d06335ad
Lyka <lykalabrada@gmail.com>
2022-11-06 12:08:06 +0800

    feature(ui-ux): wrap home components inside one container

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,13 +2,16 @@ import { NetworkProvider } from "@contexts/NetworkContext";
 import { Header } from "layouts/components/Header";
 import TokenStatsDisplay from "@components/TokenStatsDisplay";
 import GroupChainStatsCard from "@components/GroupChainStatsCard";
+import Container from "@components/commons/Container";
 
 export default function Home() {
   return (
     <NetworkProvider>
       <Header />
-      <TokenStatsDisplay />
-      <GroupChainStatsCard />
+      <Container className="px-5 sm:px-10">
+        <TokenStatsDisplay />
+        <GroupChainStatsCard />
+      </Container>
     </NetworkProvider>
   );
 }

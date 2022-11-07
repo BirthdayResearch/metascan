b50744a2590b44ac542d6a755f88dfd14ddc797f
Harsh <harshrathi.dev@gmail.com>
2022-11-07 10:28:24 +0530

    added container wrapper for large screen size

--- src/layouts/components/searchbar/SearchBar.tsx
+++ src/layouts/components/searchbar/SearchBar.tsx
@@ -5,6 +5,7 @@ import { Combobox, Transition } from "@headlessui/react";
 import clsx from "clsx";
 import { FiSearch } from "react-icons/fi";
 import { IoCloseCircleSharp } from "react-icons/io5";
+import Container from "@components/commons/Container";
 import { SearchResult, SearchResultTable } from "./SearchResult";
 
 export function SearchBar(): JSX.Element {
@@ -62,65 +63,70 @@ export function SearchBar(): JSX.Element {
   };
 
   return (
-    <Combobox value={selected} onChange={onSelect} nullable>
-      <div className="flex w-full px-5 md:px-10 lg:px-[316px] items-center justify-self-center mx-auto my-10">
-        <div
-          className={clsx(
-            "flex w-full px-8 py-[22px] rounded-lg border-[0.5px] black-gradient-1 border-black-500 focus-within:border-lightBlue black-gradient-1-shadow backdrop-blur-[6px]"
-          )}
-          data-testid="SearchBar"
-          ref={reference}
-        >
-          <div className="flex w-full">
-            <Combobox.Button as="div" className="flex w-full mr-2">
-              <FiSearch size={24} className="text-white-50 mr-2 self-center" />
-              <Combobox.Input
-                as="input"
-                onFocus={() => setIsFocused(true)}
-                onBlur={() => setIsFocused(false)}
-                placeholder={
-                  isFocused
-                    ? "Search Block / Txn / Vault ID and more"
-                    : "Search DMC blockchain ..."
-                }
-                className="h-full w-full focus:outline-none bg-transparent caret-lightBlue placeholder-black-50 text-white-50"
-                data-testid="SearchBar.Input"
-                displayValue={(item: SearchResult) => item?.title}
-                onChange={onChangeDebounceHandler}
-              />
-            </Combobox.Button>
-            {((searchString !== undefined && searchString !== "") ||
-              isFocused) && (
-              <IoCloseCircleSharp
-                size={24}
-                onClick={() => changeHandler("")}
-                className="text-white-50 self-center"
-              />
+    <Container>
+      <Combobox value={selected} onChange={onSelect} nullable>
+        <div className="flex w-full px-5 md:px-10 lg:px-[316px] items-center justify-self-center mx-auto my-10">
+          <div
+            className={clsx(
+              "flex w-full px-8 py-[22px] rounded-lg border-[0.5px] black-gradient-1 border-black-500 focus-within:border-lightBlue black-gradient-1-shadow backdrop-blur-[6px]"
             )}
-          </div>
-        </div>
-        {searchString !== undefined && searchString !== "" && (
-          <Transition className="absolute">
-            <div
-              className="z-40 mt-1"
-              ref={floating}
-              style={{
-                position: strategy,
-                top: y ?? "",
-                left: x ?? "",
-              }}
-            >
-              <div className="w-full rounded-lg border border-black-700 black-gradient-2 overflow-hidden black-gradient-1-shadow backdrop-blur-[6px]">
-                <SearchResultTable
-                  searchResults={searchResults}
-                  isSearching={isSearching}
+            data-testid="SearchBar"
+            ref={reference}
+          >
+            <div className="flex w-full">
+              <Combobox.Button as="div" className="flex w-full mr-2">
+                <FiSearch
+                  size={24}
+                  className="text-white-50 mr-2 self-center"
                 />
-              </div>
+                <Combobox.Input
+                  as="input"
+                  onFocus={() => setIsFocused(true)}
+                  onBlur={() => setIsFocused(false)}
+                  placeholder={
+                    isFocused
+                      ? "Search Block / Txn / Vault ID and more"
+                      : "Search DMC blockchain ..."
+                  }
+                  className="h-full w-full focus:outline-none bg-transparent caret-lightBlue placeholder-black-50 text-white-50"
+                  data-testid="SearchBar.Input"
+                  displayValue={(item: SearchResult) => item?.title}
+                  onChange={onChangeDebounceHandler}
+                />
+              </Combobox.Button>
+              {((searchString !== undefined && searchString !== "") ||
+                isFocused) && (
+                <IoCloseCircleSharp
+                  size={24}
+                  onClick={() => changeHandler("")}
+                  className="text-white-50 self-center"
+                />
+              )}
             </div>
-          </Transition>
-        )}
-      </div>
-    </Combobox>
+          </div>
+          {searchString !== undefined && searchString !== "" && (
+            <Transition className="absolute">
+              <div
+                className="z-40 mt-1"
+                ref={floating}
+                style={{
+                  position: strategy,
+                  top: y ?? "",
+                  left: x ?? "",
+                }}
+              >
+                <div className="w-full rounded-lg border border-black-700 black-gradient-2 overflow-hidden black-gradient-1-shadow backdrop-blur-[6px]">
+                  <SearchResultTable
+                    searchResults={searchResults}
+                    isSearching={isSearching}
+                  />
+                </div>
+              </div>
+            </Transition>
+          )}
+        </div>
+      </Combobox>
+    </Container>
   );
 }
 

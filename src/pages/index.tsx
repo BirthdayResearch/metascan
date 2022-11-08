c4d184445822a36dff8d92bc824d7f65bfa00c57
veralygit <57183851+veralygit@users.noreply.github.com>
2022-11-08 13:58:33 +0800

    update background gradient position based on figma

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -15,7 +15,7 @@ export default function Home() {
         <Footer />
       </div>
       <div className="w-full h-screen absolute z-[-1] mix-blend-screen top-0 left-0 bg-no-repeat lg:bg-cover bg-contain lg:bg-[url('/background/gradient-purple.png')] md:bg-[url('/background/gradient-purple-tablet.png')] bg-[url('/background/gradient-purple-mobile.png')]" />
-      <div className="w-full h-screen absolute top-[836px] sm:top-[210px] z-[-1] mix-blend-screen bg-no-repeat bg-contain bg-right sm:bg-[url('/background/gradient-blue-mobile.png')] bg-[url('/background/gradient-blue-mobile.png')]" />
+      <div className="w-full h-screen absolute top-[836px] sm:top-[836px] md:top-[476px] xl:top-[210px] 2xl:top-[210px] lg:top-[210px] z-[-1] mix-blend-screen bg-no-repeat bg-contain bg-right sm:bg-[url('/background/gradient-blue-mobile.png')] bg-[url('/background/gradient-blue-mobile.png')]" />
     </NetworkProvider>
   );
 }

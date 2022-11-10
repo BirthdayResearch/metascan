8de947c42a52a39561c1bd3dc5b01d0b3afe0633
Lyka <lykalabrada@gmail.com>
2022-11-10 21:52:27 +0800

    feature(ui-ux): add latest blocks table on landing page

--- src/components/commons/TransactionTable.tsx
+++ src/components/commons/TransactionTable.tsx
@@ -1,64 +1,73 @@
+import { blockTimeInSec } from "mockdata/LatestData";
 import MiddleEllipsis from "react-middle-ellipsis";
 import Button from "./Button";
 import GradientCardContainer from "./GradientCardContainer";
 import LinkText from "./LinkText";
 
-/**
- *
- */
+type DataType = "blocks" | "transactions";
+
+export interface TxnWalletInfo {
+  from: string;
+  to: string;
+}
+
+export interface BlockInfo {
+  transactionsPerBlock: string;
+  blockTimeInSec: string;
+}
+
+export interface RowData {
+  id: string;
+  transactionId: string;
+  tokenAmount: string;
+  txnOrBlockInfo: TxnWalletInfo | BlockInfo;
+  datetime: string;
+}
+
 interface Props {
-  type: "blocks" | "transactions";
+  type: DataType;
   title: string;
-  data: any[];
-  href?: string;
+  data: RowData[];
   containerClass?: string;
-  // actionBtnText: string;
-  // onActionBtnClick?: () => void;
+  amountPrefix?: string;
 }
 
 export default function TransactionTable({
   type,
   title,
   data,
-  href = "",
+  amountPrefix = "",
   containerClass = "",
-}: // actionBtnText,
-// onActionBtnClick,
-Props): JSX.Element {
+}: Props): JSX.Element {
   return (
     <div className={`pb-2 pt-6 lg:pb-6 ${containerClass ?? ""}`}>
       <GradientCardContainer>
         <div className="p-5 py-6 md:p-10">
-          {/* start of 3-section div */}
-          <div
-            className="flex flex-col 
-                      md:grid md:grid-cols-6 md:items-center"
-          >
-            {/* FIRST - TITLE */}
+          <div className="flex flex-col md:grid md:grid-cols-6 md:items-center">
             <h2 className="text-white-50 font-bold text-xl md:text-2xl leading-8 pb-4 md:pb-0 md:col-span-3">
               {title}
             </h2>
-
-            {/* SECOND - MAIN TABLE */}
             <div className="md:order-last md:flex-1 md:pt-6 md:col-span-8">
               {data.map((row) => (
-                <RowItem key={row.id} rowData={row} />
+                <RowItem
+                  key={row.id}
+                  type={type}
+                  rowData={row}
+                  amountPrefix={amountPrefix}
+                />
               ))}
             </div>
-
-            {/* THIRD - ACTION BUTTON */}
             <div className="pt-5 md:pt-0 md:col-span-3 md:col-end-auto">
               <div className="md:flex md:justify-end md:text-rightt">
                 <Button
                   testId=""
                   label={`View all ${type}`}
-                  href={href}
+                  href={`/${type}`}
                   customStyle="w-full md:w-auto"
                 />
               </div>
             </div>
           </div>
-          {/* end of 3-section div */}
         </div>
       </GradientCardContainer>
     </div>
@@ -66,41 +75,92 @@ Props): JSX.Element {
 }
 
 const DmxTokenSymbol = "DMXTc";
-function RowItem({ rowData }: { rowData: any }) {
+function RowItem({
+  type,
+  rowData,
+  amountPrefix,
+}: {
+  type: DataType;
+  rowData: RowData;
+  amountPrefix: string;
+}): JSX.Element {
+  const { transactionId, tokenAmount, datetime, txnOrBlockInfo } = rowData;
+  const detailsPageLink = `/${type}/${transactionId}`;
   return (
-    <div
-      className="text-white-50 py-5 border-b border-gray-1000/50
-                    md:flex md:flex-wrap md:items-start lg:flex-nowrap xl:gap-5"
-    >
-      <div className="w-2/4 inline-flex items-center md:w-3/12 lg:w-36">
+    <div className="text-white-50 py-5 border-b border-gray-1000/50 md:flex md:flex-wrap md:items-start lg:flex-nowrap xl:gap-5">
+      <div className="w-2/4 inline-flex items-center md:w-32 lg:w-36">
         <MiddleEllipsis>
-          <LinkText href="#" label={rowData.transactionId} />
+          <LinkText href={detailsPageLink} label={transactionId} />
         </MiddleEllipsis>
       </div>
       <div className="w-2/4 inline-flex items-center justify-end text-white-700 text-right md:order-last md:grow md:-mt-6 lg:mt-0 lg:w-20 xl:w-32 lg:pl-5 xl:pl-0">
-        {rowData.datetime}
+        {datetime}
       </div>
-      <div className="md:w-5/12 lg:flex lg:w-96 lg:pl-12 xl:gap-5">
-        <div className="flex pt-5 md:pt-0 lg:w-48">
-          <span className="pr-1">From: </span>
-          <div className="w-4/5 lg:w-36">
-            <MiddleEllipsis>
-              <LinkText href="#" label={rowData.from} />
-            </MiddleEllipsis>
-          </div>
-        </div>
-        <div className="flex pt-1.5 lg:pt-0 lg:w-48">
-          <span className="pr-1">To: </span>
-          <div className="w-4/5 lg:w-36">
-            <MiddleEllipsis>
-              <LinkText href="#" label={rowData.to} />
-            </MiddleEllipsis>
-          </div>
-        </div>
+      <div className="md:w-4/12 lg:flex lg:w-96 lg:pl-12">
+        {type === "blocks" ? (
+          <BlockInfoDisplay
+            block={transactionId}
+            blockInfo={txnOrBlockInfo as BlockInfo}
+          />
+        ) : (
+          <TxnWalletInfoDisplay txnInfo={txnOrBlockInfo as TxnWalletInfo} />
+        )}
       </div>
       <div className="pt-5 pr-1 md:grow md:text-right md:p-0 lg:grow xl:w-2/5">
-        {rowData.amount} {DmxTokenSymbol}
+        {amountPrefix && (
+          <span className="text-white-700">{amountPrefix}&nbsp;</span>
+        )}
+        {tokenAmount} {DmxTokenSymbol}
       </div>
     </div>
+  );
+}
+
+function BlockInfoDisplay({
+  block,
+  blockInfo,
+}: {
+  block: string;
+  blockInfo: BlockInfo;
+}): JSX.Element {
+  return (
+    <>
+      <div className="flex pt-5 md:pt-0">
+        <LinkText
+          href={`/transactions?block=${block}`}
+          label={`${blockInfo.transactionsPerBlock} Transactions`}
+        />
+      </div>
+      <div className="flex pt-1.5 lg:pt-0">
+        &nbsp;{`in ${blockTimeInSec} sec`}
+      </div>
+    </>
+  );
+}
+
+function TxnWalletInfoDisplay({
+  txnInfo,
+}: {
+  txnInfo: TxnWalletInfo;
+}): JSX.Element {
+  return (
+    <>
+      <div className="flex pt-5 md:pt-0 lg:w-48">
+        <span className="pr-1">From: </span>
+        <div className="w-4/5 lg:w-36">
+          <MiddleEllipsis>
+            <LinkText href={`/address/${txnInfo.from}`} label={txnInfo.from} />
+          </MiddleEllipsis>
+        </div>
+      </div>
+      <div className="flex pt-1.5 lg:pt-0 lg:w-48  xl:ml-11">
+        <span className="pr-1">To: </span>
+        <div className="w-4/5 lg:w-36">
+          <MiddleEllipsis>
+            <LinkText href={`/address/${txnInfo.to}`} label={txnInfo.to} />
+          </MiddleEllipsis>
+        </div>
+      </div>
+    </>
   );
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -6,51 +6,108 @@ import TransactionTable from "@components/commons/TransactionTable";
 
 const latestTransactions = [
   {
-    id: 10001,
-    transactionId:
+    id: "1",
+    transactionHash:
       "698bfdd7a59e4b5cd01793b5a5d84fd40f918aee7e44babe3e00591b7462dd4c",
     from: "1MtKcRsyJrgUuek1oERFmzgbeZDNhuJcyH",
     to: "14LLip4BdVEpQbCACYU2Z258Ag9wXMjuHR",
-    amount: 40534.50833603,
+    amount: "40534.50833603",
     datetime: "4s ago",
   },
   {
-    id: 10002,
-    transactionId:
+    id: "2",
+    transactionHash:
       "bc33c99e62d66723920ead7163877b3fb36a82c17f8a3ce0ce8102c8be6a7f8f",
     from: "1B75Sidfan9KfvW7YNw7FyyztzCXH1TCNw",
     to: "18iBw84Au7tySsSL5myZP4Eoj1gKhXryxP",
-    amount: 59730.4548642,
+    amount: "59730.4548642",
     datetime: "35s ago",
   },
   {
-    id: 10003,
-    transactionId:
+    id: "3",
+    transactionHash:
       "abfc697a26ee7d2a5a4ef96de8286b7d93d91eb7c8157e0c6e5532d5b42827fa",
     from: "1HXgJYbT3k2JZypbPw7unmhTV7YGLEskLU",
     to: "18XdYkTJFnAZXjam14rMpiBWw9h5Qz1jx9",
-    amount: 193396.91258727,
+    amount: "193396.91258727",
     datetime: "50s ago",
   },
   {
-    id: 10004,
-    transactionId:
+    id: "4",
+    transactionHash:
       "bc33c99e62d66723920ead7163877b3fb36a82c17f8a3ce0ce8102c8be6a7f8f",
     from: "1P63Sq1QigEyFZ3ck8juKZtX1t99R6ubTs",
     to: "1Q4Y9KpV8V3ieXBuJgVw2wuR7GVpRddxmQ",
-    amount: 53111.48728868,
+    amount: "53111.48728868",
     datetime: "1m ago",
   },
   {
-    id: 10005,
-    transactionId:
+    id: "5",
+    transactionHash:
       "eb04ba75f26df54de20b5957cc06a769cf6a84d2ef1a28f9dc4d14a0b8febdb2",
     from: "1GyQ7wso8xPXejSwQKMdB2aek7mjSyXCwE",
     to: "15pNaHJXgmfxnrHqEoZutJ7oGxKWe8v815",
-    amount: 13786.81228866,
+    amount: "13786.81228866",
     datetime: "10m ago",
   },
 ];
+
+const blockTimeInSec = "22";
+const latestBlocks = [
+  {
+    id: "1",
+    blockHeight: "15939349",
+    transactionsPerBlock: "40534",
+    rewardAmount: "12.21210023",
+    datetime: "8s ago",
+  },
+  {
+    id: "2",
+    blockHeight: "15939348",
+    transactionsPerBlock: "4548642",
+    rewardAmount: "12.21210023",
+    datetime: "41s ago",
+  },
+  {
+    id: "3",
+    blockHeight: "15939347",
+    transactionsPerBlock: "193396",
+    rewardAmount: "12.21210023",
+    datetime: "54s ago",
+  },
+  {
+    id: "4",
+    blockHeight: "15939346",
+    transactionsPerBlock: "53111",
+    rewardAmount: "12.21210023",
+    datetime: "3m ago",
+  },
+  {
+    id: "5",
+    blockHeight: "15939345",
+    transactionsPerBlock: "13786",
+    rewardAmount: "12.21210023",
+    datetime: "12m ago",
+  },
+];
+const mappedBlocks = latestBlocks.map((b) => ({
+  id: b.id,
+  transactionId: b.blockHeight,
+  tokenAmount: b.rewardAmount,
+  txnOrBlockInfo: {
+    transactionsPerBlock: b.transactionsPerBlock,
+    blockTimeInSec,
+  },
+  datetime: b.datetime,
+}));
+
+const mappedransactions = latestTransactions.map((t) => ({
+  id: t.id,
+  transactionId: t.transactionHash,
+  tokenAmount: t.amount,
+  txnOrBlockInfo: { from: t.from, to: t.to },
+  datetime: t.datetime,
+}));
 
 export default function Home() {
   return (
@@ -63,14 +120,13 @@ export default function Home() {
         <TransactionTable
           type="blocks"
           title="Latest blocks"
-          href="/blocks"
-          data={latestTransactions}
+          data={mappedBlocks}
+          amountPrefix="Reward:"
         />
         <TransactionTable
           type="transactions"
           title="Latest transactions"
-          href="/transactions"
-          data={latestTransactions}
+          data={mappedransactions}
         />
       </div>
     </>

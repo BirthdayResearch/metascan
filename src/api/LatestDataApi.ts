460707b57826687f04741cb4f336b167f15bbc24
Lyka <lykalabrada@gmail.com>
2022-11-11 12:02:31 +0800

    feature(ui-ux): add todos in the data formatting

--- src/api/LatestDataApi.ts
+++ src/api/LatestDataApi.ts
@@ -5,19 +5,19 @@ export default {
     mockData.latestBlocks.map((b) => ({
       id: b.id,
       transactionId: b.blockHeight,
-      tokenAmount: b.rewardAmount,
+      tokenAmount: b.rewardAmount, // TODO: Format amount to have commas accordingly
       txnOrBlockInfo: {
         transactionsPerBlock: b.transactionsPerBlock,
         blockTimeInSec: mockData.blockTimeInSec,
       },
-      datetime: b.datetime,
+      datetime: b.datetime, // TODO: Format time into 's ago, m ago, etc..' once real data is ready
     })),
   useLatestTransactions: () =>
     mockData.latestTransactions.map((t) => ({
       id: t.id,
       transactionId: t.transactionHash,
-      tokenAmount: t.amount,
+      tokenAmount: t.amount, // TODO: Format amount to have commas accordingly
       txnOrBlockInfo: { from: t.from, to: t.to },
-      datetime: t.datetime,
+      datetime: t.datetime, // TODO: Format time into 's ago, m ago, etc..' once real data is ready
     })),
 };

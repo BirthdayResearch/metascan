0952e868b3c2daf233eea37503edd89e03f1d890
veralygit <57183851+veralygit@users.noreply.github.com>
2022-11-18 09:46:48 +0800

    update path routing to tx instead of txs

--- src/layouts/components/searchbar/SearchBar.tsx
+++ src/layouts/components/searchbar/SearchBar.tsx
@@ -151,7 +151,7 @@ async function getSearchResults(query): Promise<SearchResult[]> {
     {
       title: query,
       type: "Transaction",
-      url: `/txs/${query}`,
+      url: `/tx/${query}`,
     },
     {
       title: query,

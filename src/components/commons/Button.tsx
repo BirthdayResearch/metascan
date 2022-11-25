be285767f011c3b3f8e4c02939e14a8d2ec427cf
veralygit <57183851+veralygit@users.noreply.github.com>
2022-11-25 14:01:17 +0800

    fix(ui-ux): update button to open on current tab instead of new tab (#72)
    * fix(ui-ux): update button to open on current tab instead of new tab
    
    * update custom button to be able to show link and on active states
    
    * update button disabled back to false
    
    * update button component to have href wrapper for link preview
    
    * remove unused styles from global styles
    
    * update button stylings
    
    * update button disabled stylings
    
    * update disable back to default false

--- src/components/commons/Button.tsx
+++ src/components/commons/Button.tsx
@@ -1,3 +1,5 @@
+import clsx from "clsx";
+
 type ButtonSize = "small" | "medium" | "large";
 
 interface ButtonProps {
@@ -36,26 +38,29 @@ export default function Button({
   const handleButtonClick = () => {
     if (onClick) {
       onClick();
-      return;
     }
-    window.open(href, "_blank", "noreferrer");
   };
 
   const transitionStyle = "transition-all ease-in duration-300";
+
   return (
-    <button
-      data-testid={`${testId}-button`}
-      type="button"
-      className={`flex items-center justify-center rounded-[28px] group border border-white-50 brand-bg-gradient-1 active:brand-bg-gradient-2 hover:border-transparent
-                  ${transitionStyle} ${btnPadding} ${customStyle ?? ""}`}
-      disabled={disabled}
-      onClick={handleButtonClick}
-    >
-      <span
-        className={`text-white-50 brand-gradient-1 group-active:brand-gradient-2 bg-clip-text group-hover:text-transparent ${transitionStyle}`}
+    <a href={href} className={clsx({ "pointer-events-none": disabled })}>
+      <button
+        data-testid={`${testId}-button`}
+        type="button"
+        className={`flex items-center justify-center rounded-[28px] group border border-white-50 brand-bg-gradient-1 active:brand-bg-gradient-2 hover:border-transparent
+                  ${transitionStyle} ${btnPadding} ${customStyle ?? ""} ${
+          disabled ? "opacity-50" : "opacity-100"
+        }`}
+        disabled={disabled}
+        onClick={handleButtonClick}
       >
-        {label}
-      </span>
-    </button>
+        <span
+          className={`text-white-50 font-medium tracking-[0.02em] brand-gradient-1 group-active:brand-gradient-2 bg-clip-text group-hover:text-transparent ${transitionStyle} `}
+        >
+          {label}
+        </span>
+      </button>
+    </a>
   );
 }

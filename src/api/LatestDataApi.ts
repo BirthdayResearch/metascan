36ab11046d132b4bbb7764af01901d5240f8f0fd
Lyka Labrada <lykalabrada@gmail.com>
2023-04-26 11:10:08 +0800

    chore(api): integrate blockscout api for latest data (#147) (#148)
    * chore(api): integrate blockscout latest transaction api
    
    * chore(api): integrate blockscout latest block api, refactor latest api integration
    
    Co-authored-by: JoshuaC817 <josh_817@hotmail.com>

--- src/api/LatestDataApi.ts
+++ src/api/LatestDataApi.ts
@@ -1,28 +1,58 @@
-import { transactions } from "mockdata/TransactionData";
-import mockData from "../mockdata/LatestData";
+import { TransactionType } from "mockdata/TransactionData";
+import dayjs from "dayjs";
+import { RowData } from "@components/LatestDataTable";
+import { MAIN_LATEST_BLOCK_URL, MAIN_LATEST_TRANSACTION_URL } from "./index";
+
+const MAX_ROW = 5;
 
 export default {
-  useLatestBlocks: () =>
-    mockData.latestBlocks.map((b) => ({
-      id: b.id,
-      transactionId: b.blockHeight,
-      tokenAmount: b.rewardAmount,
-      txnOrBlockInfo: {
-        transactionsPerBlock: b.transactionsPerBlock,
-        blockTimeInSec: mockData.blockTimeInSec,
-      },
-      time: b.time,
-    })),
-  useLatestTransactions: () =>
-    transactions.slice(0, 5).map((t, i) => ({
-      id: `${i + 1}`,
-      transactionId: t.hash,
-      tokenAmount: t.amount,
-      txnOrBlockInfo: {
-        from: t.from,
-        to: t.to,
-        transactionType: t.transactionType,
-      },
-      time: t.time,
-    })),
+  getLatestBlocks: async (): Promise<RowData[]> => {
+    const resBlock = await fetch(MAIN_LATEST_BLOCK_URL);
+    const responseBlockData = await resBlock.json();
+    const blockRows = Math.min(responseBlockData.length, MAX_ROW);
+
+    return responseBlockData.slice(0, blockRows).map((data) => {
+      const reward =
+        data.rewards !== undefined && data.rewards.length > 0
+          ? data.rewards[0].reward
+          : 0;
+      const time = dayjs().unix() - dayjs(data.timestamp).unix();
+      return {
+        transactionId: data.height,
+        tokenAmount: reward,
+        txnOrBlockInfo: {
+          transactionsPerBlock: data.tx_count || null,
+          blockTimeInSec: null,
+        },
+        time,
+      };
+    });
+  },
+  getLatestTransactions: async (): Promise<RowData[]> => {
+    const resTxn = await fetch(MAIN_LATEST_TRANSACTION_URL);
+    const responseTxnData = await resTxn.json();
+    const txnRows = Math.min(responseTxnData.length, MAX_ROW);
+
+    return responseTxnData.slice(0, txnRows).map((data) => {
+      // TODO temporary workaround to display txn type icons
+      const type: string | undefined =
+        data.tx_types !== undefined && data.tx_types.length > 0
+          ? data.tx_types[0]
+          : undefined;
+      const transactionType = type?.includes("contract")
+        ? TransactionType.ContractCall
+        : TransactionType.Transaction;
+      const time = dayjs().unix() - dayjs(data.timestamp).unix();
+      return {
+        transactionId: data.hash,
+        tokenAmount: data.value,
+        txnOrBlockInfo: {
+          from: data.from.hash,
+          to: data.to?.hash || null,
+          transactionType,
+        },
+        time,
+      };
+    });
+  },
 };
---
+++ src/api/index.ts
@@ -0,0 +1,4 @@
+const BLOCKSCOUT_ENDPOINT = "https://base-goerli.blockscout.com/";
+
+export const MAIN_LATEST_TRANSACTION_URL = `${BLOCKSCOUT_ENDPOINT}api/v2/main-page/transactions`;
+export const MAIN_LATEST_BLOCK_URL = `${BLOCKSCOUT_ENDPOINT}api/v2/main-page/blocks`;

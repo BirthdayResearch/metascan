e2b2435f062283f359678849c10b9f20c184ecb9
joshua <josh_817@hotmail.com>
2023-05-03 16:33:43 +0800

    chore(ops): add security header to csp

--- next.config.js
+++ next.config.js
@@ -1,7 +1,59 @@
-/** @type {import('next').NextConfig} */
+const securityHeaders = [
+  {
+    key: "Content-Security-Policy",
+    value:
+      `default-src 'none';` +
+      `base-uri 'none';` +
+      `child-src 'self' app.netlify.com;` +
+      `form-action 'none';` +
+      `frame-ancestors 'none';` +
+      `img-src 'self' data:;` +
+      `media-src 'self';` +
+      `object-src 'none';` +
+      `script-src 'self' app.netlify.com netlify-cdp-loader.netlify.app ${
+        process.env.NODE_ENV === "development" ? `'unsafe-eval'` : ""
+      };` +
+      `style-src 'self' fonts.googleapis.com 'unsafe-inline';` +
+      `font-src fonts.gstatic.com;` +
+      `connect-src 'self' base-goerli.blockscout.com eth-goerli.blockscout.com ${
+        process.env.NODE_ENV === "development"
+          ? `ws://localhost:3000/_next/webpack-hmr`
+          : ""
+      };`,
+  },
+  {
+    key: "Referrer-Policy",
+    value: "same-origin",
+  },
+  {
+    key: "X-Content-Type-Options",
+    value: "nosniff",
+  },
+  {
+    key: "X-Frame-Options",
+    value: "DENY",
+  },
+  {
+    key: "X-XSS-Protection",
+    value: "1; mode=block",
+  },
+  {
+    key: "Strict-Transport-Security",
+    value: "max-age=63072000; includeSubDomains; preload",
+  },
+];
+
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+  async headers() {
+    return [
+      {
+        source: "/(.*)",
+        headers: securityHeaders,
+      },
+    ];
+  },
 };
 
 module.exports = nextConfig;

9fc06f5f0a605654e5aa1ab706e136d1444a3d8c
JJ Adonis <work@josejai.me>
2023-05-22 16:42:10 +0800

    chore(ops): bump deps (#161)
    * chore(ops): bump deps
    
    * chore(ops): bump deps

--- next.config.js
+++ next.config.js
@@ -1,7 +1,63 @@
+const securityHeaders = [
+  {
+    key: "Content-Security-Policy",
+    value:
+      `default-src 'none';` +
+      `base-uri 'self';` +
+      `child-src 'self' app.netlify.com;` +
+      `form-action 'none';` +
+      `frame-ancestors 'none';` +
+      `img-src 'self' images.prismic.io data:;` +
+      `media-src 'self';` +
+      `object-src 'none';` +
+      `script-src 'self' app.netlify.com netlify-cdp-loader.netlify.app ${
+        process.env.NODE_ENV === "development" ? `'unsafe-eval'` : ""
+      };` +
+      `style-src 'self' fonts.googleapis.com 'unsafe-inline';` +
+      `font-src fonts.gstatic.com;` +
+      `connect-src 'self' base-goerli.blockscout.com eth-goerli.blockscout.com ${
+        process.env.NODE_ENV === "development"
+          ? `ws://localhost:3000/_next/webpack-hmr`
+          : ""
+      };`,
+  },
+  {
+    key: "Referrer-Policy",
+    value: "same-origin",
+  },
+  {
+    key: "X-Content-Type-Options",
+    value: "nosniff",
+  },
+  {
+    key: "X-Frame-Options",
+    value: "DENY",
+  },
+  {
+    key: "X-XSS-Protection",
+    value: "1; mode=block",
+  },
+  {
+    key: "Strict-Transport-Security",
+    value: "max-age=63072000; includeSubDomains; preload",
+  },
+];
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+  images: {
+    deviceSizes: [360, 768, 1440, 1920, 2048, 3840],
+  },
+  async headers() {
+    return [
+      {
+        source: "/:path*",
+        headers: securityHeaders,
+      },
+    ];
+  },
 };
 
 module.exports = nextConfig;

866aadf518dfe6875fe8560bc80848a3ab5b9709
pierregee <pierre@cakedefi.com>
2023-05-26 14:16:27 +0800

    add try catch

--- src/pages/txs/index.tsx
+++ src/pages/txs/index.tsx
@@ -75,20 +75,24 @@ export async function getServerSideProps(
     !isNumeric(params?.page_number as string) ||
     !isNumeric(params?.index as string);
 
-  // Fetch data from external API
-  const txs = hasInvalidParams
-    ? await TransactionsApi.getTransactions(network as NetworkConnection)
-    : await TransactionsApi.getTransactions(
-        network as NetworkConnection,
-        params?.block_number as string,
-        params?.items_count as string,
-        params?.index as string
-      );
-  const data = {
-    transactions: txs.items,
-    next_page_params: txs.next_page_params as TxnNextPageParamsProps,
-  };
+  try {
+    // Fetch data from external API
+    const txs = hasInvalidParams
+      ? await TransactionsApi.getTransactions(network as NetworkConnection)
+      : await TransactionsApi.getTransactions(
+          network as NetworkConnection,
+          params?.block_number as string,
+          params?.items_count as string,
+          params?.index as string
+        );
+    const data = {
+      transactions: txs.items,
+      next_page_params: txs.next_page_params as TxnNextPageParamsProps,
+    };
 
-  // Pass data to the page via props
-  return { props: { data } };
+    // Pass data to the page via props
+    return { props: { data } };
+  } catch (e) {
+    return { notFound: true };
+  }
 }

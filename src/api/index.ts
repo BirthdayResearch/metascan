82f5bf595a342e5054653a368b00306e376c81c6
Lyka Labrada <lykalabrada@gmail.com>
2023-06-01 12:05:42 +0800

    feat(core): move rpc urls to env (#177)

--- src/api/index.ts
+++ src/api/index.ts
@@ -8,9 +8,6 @@ export function wrapResponse<T>(res: Response): Promise<T> {
   return res.json();
 }
 
-const BLOCKSCOUT_ENDPOINT_MAINNET = "https://base-goerli.blockscout.com"; // TODO: Replace with MainNet blockscout URL
-const BLOCKSCOUT_ENDPOINT_TESTNET = "https://eth-goerli.blockscout.com"; // TODO: Replace with TestNet blockscout URL
-
 export const MAIN_LATEST_TRANSACTION_URL = "api/v2/main-page/transactions";
 export const MAIN_LATEST_BLOCK_URL = "api/v2/main-page/blocks";
 export const MAIN_BLOCKS_URL = "api/v2/blocks";
@@ -19,9 +16,9 @@ export const WALLET_ADDRESS_URL = "api/v2/addresses";
 
 export const getBaseUrl = (network: NetworkConnection) => {
   if (network === NetworkConnection.TestNet) {
-    return BLOCKSCOUT_ENDPOINT_TESTNET;
+    return process.env.RPC_URL_TESTNET;
   }
-  return BLOCKSCOUT_ENDPOINT_MAINNET;
+  return process.env.RPC_URL_MAINNET;
 };
 
 export function filterParams(params: { key: string; value }[]): string {

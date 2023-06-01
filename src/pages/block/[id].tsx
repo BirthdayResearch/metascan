530cc2718512bc235cfce9c68f9758f95da6f43a
Pierre Gee <pierre.gabaldon@gmail.com>
2023-06-01 10:15:28 +0800

    feat(ui-ux): add txn list on block detail page (#169)
    * feat(ui-ux): add txn list on block detail page
    
    * add comment on messy repetitive code
    
    * feature(ui-ux): add pagination
    
    * fix(ui-ux): lint errors
    
    * feature(ui-ux): cleanup prop naming
    
    * chore(ui-ux): merge repetitive types
    
    * improve naming
    
    * fix typos
    
    * revert url change

--- src/pages/block/[id].tsx
+++ src/pages/block/[id].tsx
@@ -7,24 +7,35 @@ import clsx from "clsx";
 import useCopyToClipboard from "hooks/useCopyToClipboard";
 import { SearchBar } from "layouts/components/searchbar/SearchBar";
 import { useRouter } from "next/router";
-import { GetServerSidePropsContext } from "next";
+import {
+  GetServerSidePropsContext,
+  GetServerSidePropsResult,
+  InferGetServerSidePropsType,
+} from "next";
 import { useEffect, useState } from "react";
 import { FiArrowLeft, FiArrowRight, FiCopy } from "react-icons/fi";
 import { MdCheckCircle } from "react-icons/md";
 import { getDuration, getTimeAgo } from "shared/durationHelper";
-import { truncateTextFromMiddle } from "shared/textHelper";
+import { isNumeric, truncateTextFromMiddle } from "shared/textHelper";
 import { getRewards } from "shared/getRewards";
 import { NetworkConnection } from "@contexts/Environment";
 import BlocksApi from "@api/BlocksApi";
-import { BlockProps } from "@api/types";
+import { TxnNextPageParamsProps } from "@api/TransactionsApi";
+import { BlockProps, RawTransactionI } from "@api/types";
+import BlockTransactionList from "./_components/BlockTransactionList";
 
-// TODO: Replace `any` with proper types
-interface Props {
+interface PageProps {
   block: BlockProps;
-  // blockTransactions: any;
+  blockTransactions: {
+    transactions: RawTransactionI[];
+    nextPageParams: TxnNextPageParamsProps;
+  };
 }
 
-export default function Block({ block }: Props) {
+export default function Block({
+  data: { block, blockTransactions },
+  isLoading,
+}: InferGetServerSidePropsType<typeof getServerSideProps>) {
   const router = useRouter();
   const blockNumber = new BigNumber(router.query.id as string);
   const prevBlockNumber = blockNumber.minus(1);
@@ -144,13 +155,14 @@ export default function Block({ block }: Props) {
       </GradientCardContainer>
 
       {/* Block transaction list */}
-      {/* <div data-testid="block-transaction-list" className="mt-6">
+      <div data-testid="block-transaction-list" className="mt-6">
         <BlockTransactionList
           blockNumber={blockNumber.toFixed(0)}
-          blockTransactions={data.blockTransactions}
-          pages={data.pages}
+          transactions={blockTransactions.transactions}
+          nextPageParams={blockTransactions.nextPageParams}
+          isLoading={isLoading}
         />
-      </div> */}
+      </div>
     </div>
   );
 }
@@ -287,18 +299,54 @@ function GasUsedRow({
   );
 }
 
-export async function getServerSideProps(context: GetServerSidePropsContext) {
+export async function getServerSideProps(
+  context: GetServerSidePropsContext
+): Promise<GetServerSidePropsResult<{ data: PageProps; isLoading?: boolean }>> {
+  const { network, ...params } = context.query;
+
   if (context.params?.id === undefined) {
-    return null;
+    return { notFound: true };
   }
+
+  const blockId = context.params?.id?.toString().trim() as string;
 
   try {
     const block = await BlocksApi.getBlock(
-      context.query.network as NetworkConnection,
-      context.params.id as string
+      network as NetworkConnection,
+      blockId
     );
 
-    return { props: { block } };
+    const hasInvalidParams =
+      !isNumeric(params?.block_number as string) ||
+      !isNumeric(params?.items_count as string) ||
+      !isNumeric(params?.page_number as string) ||
+      !isNumeric(params?.index as string);
+
+    const blockTransactions = hasInvalidParams
+      ? await BlocksApi.getBlockTransactions(
+          network as NetworkConnection,
+          blockId
+        )
+      : await BlocksApi.getBlockTransactions(
+          network as NetworkConnection,
+          blockId,
+          params?.block_number as string,
+          params?.items_count as string,
+          params?.index as string
+        );
+
+    return {
+      props: {
+        data: {
+          block,
+          blockTransactions: {
+            transactions: blockTransactions.items,
+            nextPageParams:
+              blockTransactions.next_page_params as TxnNextPageParamsProps,
+          },
+        },
+      },
+    };
   } catch (e) {
     return { notFound: true };
   }

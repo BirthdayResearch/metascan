45d0b28399e80f12867ae6055eb88d6ecab1edc5
Lyka Labrada <lykalabrada@gmail.com>
2023-06-06 00:05:00 +0800

    update textarea scrollbar

--- src/pages/tx/_components/RawInput.tsx
+++ src/pages/tx/_components/RawInput.tsx
@@ -1,13 +1,23 @@
 import clsx from "clsx";
-import { useState } from "react";
+import { useEffect, useState, useRef } from "react";
 
 import BoldedTitle from "./BoldedTitle";
 
 export default function RawInput({ hex }: { hex: string }) {
+  const textareaRef = useRef<HTMLTextAreaElement>(null);
+  const [allowResize, setAllowResize] = useState(false);
   const [isRawInputExpanded, setIsRawInputExpanded] = useState(false);
   const onRawInputClick = () => {
     setIsRawInputExpanded(!isRawInputExpanded);
   };
+
+  useEffect(() => {
+    const scrollHeight = textareaRef.current?.scrollHeight ?? 0;
+    const clientHeight = textareaRef.current?.clientHeight ?? 0;
+    if (scrollHeight > clientHeight) {
+      setAllowResize(true);
+    }
+  }, [textareaRef]);
 
   return (
     <div>
@@ -29,25 +39,21 @@ export default function RawInput({ hex }: { hex: string }) {
         </div>
       </div>
       <textarea
+        ref={textareaRef}
         data-testid="transaction-hex"
-        rows={3}
         className={clsx(
-          "textarea-scrollbar w-full transition-opacity rounded-md font-space-mono tracking-[-0.04em] break-all bg-black-800 border-[1px] border-black-600 text-white-50 text-xs",
-          "p-4",
-          "lg:px-10"
+          "textarea-scrollbar w-full transition-opacity rounded-md font-space-mono tracking-[-0.04em] break-all bg-black-800 border-[1px] border-black-600 text-white-50 text-xs p-4 lg:px-10",
+          allowResize ? "resize-y" : "resize-none"
         )}
         disabled
-      >
-        {hex}
-      </textarea>
-      <div
-        className={clsx(
-          "relative inline-block after:bg-white-900 after:z-[1] after:absolute after:right-[2px] after:bottom-[12px]",
-          "after:pointer-events-none after:rounded-md after:w-[19.8px] after:border-t-[1.5px] after:border-white-900 after:rotate-[135deg]"
-        )}
-      >
-        <div className="absolute border-t-[1.5px] border-white-900 rounded-md w-[11.31px] right-[2px] bottom-2 rotate-[135deg] z-[2]" />
-      </div>
+        value={hex}
+      />
+      {/* custom style for scrollbar resizer */}
+      {allowResize && (
+        <div className="relative inline-block after:bg-white-900 after:z-[1] after:absolute after:right-[2px] after:bottom-[12px] after:pointer-events-none after:rounded-md after:w-[19.8px] after:border-t-[1.5px] after:border-white-900 after:rotate-[135deg]">
+          <div className="absolute border-t-[1.5px] border-white-900 rounded-md w-[11.31px] right-[2px] bottom-2 rotate-[135deg] z-[2]" />
+        </div>
+      )}
     </div>
   );
 }

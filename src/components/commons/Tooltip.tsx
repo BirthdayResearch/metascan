996e54a871da0bb922b2145c4eaf210a4f942c8a
pierregee <pierre@cakedefi.com>
2023-06-08 16:30:22 +0800

    update design

--- src/components/commons/Tooltip.tsx
+++ src/components/commons/Tooltip.tsx
@@ -16,11 +16,11 @@ export default function Tooltip({
     <div className="group relative">
       <div
         className={clsx(
-          "pointer-events-none absolute -top-10 left-1/2 -translate-x-1/2 whitespace-nowrap rounded black-gradient-1 text-xs text-white-50 px-4 py-2 text-white opacity-0 transition before:absolute before:left-1/2 before:top-full before:-translate-x-1/2 before:border-4 before:border-transparent before:border-t-black before:content-[''] group-hover:opacity-100",
+          "font-semibold pointer-events-none absolute -top-10 left-5 -translate-x-1/2 whitespace-nowrap rounded bg-dark-200 text-xs text-white-50 px-4 py-2 text-white opacity-0 transition before:absolute before:left-1/2 before:top-full before:-translate-x-1/2 before:border-4 before:border-transparent before:border-t-black before:content-[''] group-hover:opacity-100",
           { hidden: !active }
         )}
       >
-        <div className=" h-2 w-2 absolute rotate-45 -bottom-1 left-[46%] black-gradient-1" />
+        <div className="h-2 w-2 absolute rotate-45 -bottom-1 left-[46%] bg-dark-200" />
         {text}
       </div>
       {children}

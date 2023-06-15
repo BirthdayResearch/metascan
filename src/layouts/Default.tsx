85629aa4ffe97911900fb0a1293a52c86bddf46f
pierregee <pierre@cakedefi.com>
2023-06-15 19:30:01 +0800

    remove dup

--- src/layouts/Default.tsx
+++ src/layouts/Default.tsx
@@ -63,7 +63,6 @@ export function Default({ children }: PropsWithChildren): JSX.Element {
           content="https://meta.defiscan.live/metascan_share.png"
         />
         <meta name="twitter:image:alt" content={title} />
-        <meta name="twitter:card" content="summary_large_image" />
 
         <link rel="icon" href="/favicon.ico" />
         <link rel="icon" type="image/png" sizes="48x48" href="/favicon.png" />

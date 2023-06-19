caf1c83d6488e27533df3c7f20aa3c69a3b695af
pierregee <pierre@cakedefi.com>
2023-06-19 15:16:56 +0800

    feat(ui-ux): add api integration on contract detail page

--- src/pages/contract/[cid].tsx
+++ src/pages/contract/[cid].tsx
@@ -3,25 +3,48 @@ import LinkText from "@components/commons/LinkText";
 import NumericFormat from "@components/commons/NumericFormat";
 import { GreenTickIcon } from "@components/icons/GreenTickIcon";
 import { SearchBar } from "layouts/components/searchbar/SearchBar";
-import { readContractPages, verifiedContractData } from "mockdata/ContractData";
-import { pages, transactions } from "mockdata/TransactionData";
-import { tokenPages, tokens } from "mockdata/TokenData";
 import { useRouter } from "next/router";
 import { Dispatch, SetStateAction, useState } from "react";
 import { FiCopy } from "react-icons/fi";
 import { MdOutlineQrCode } from "react-icons/md";
-import { truncateTextFromMiddle } from "shared/textHelper";
+import {
+  isAlphanumeric,
+  isNumeric,
+  truncateTextFromMiddle,
+} from "shared/textHelper";
 import QrCode from "@components/commons/QrCode";
 import VerifiedGreenTickIcon from "@components/icons/VerifiedGreenTickIcon";
 import { sleep } from "shared/sleep";
 import { ContractTabsTitle } from "enum/contractTabsTitle";
-import ContractTabs from "./_components/ContractTabs";
-import ContractTokensList from "./_components/ContractTokensList";
-import ContractTransactionsList from "./_components/ContractTransactionsList";
+import {
+  GetServerSidePropsContext,
+  GetServerSidePropsResult,
+  InferGetServerSidePropsType,
+} from "next";
+import { TxnNextPageParamsProps } from "@api/TransactionsApi";
+import { utils } from "ethers";
+import WalletAddressApi from "@api/WalletAddressApi";
+import { NetworkConnection } from "@contexts/Environment";
+import { DMX_TOKEN_SYMBOL } from "shared/constants";
+import { AddressTransactionsProps } from "pages/address/_components/WalletDetails";
+import TransactionDetails from "@components/TransactionDetails";
+import { WalletAddressInfoI } from "@api/types";
 import VerifiedContractSubtitle from "./_components/VerifiedContractSubtitle";
-import ContractCode from "./_components/ContractCode";
+import ContractTabs from "./_components/ContractTabs";
 
-export default function VerifiedContract({ data }) {
+interface ContractDetailProps {
+  addressTransactions: AddressTransactionsProps;
+  balance: string;
+  detail: WalletAddressInfoI;
+  isLoading?: boolean;
+}
+
+export default function VerifiedContract({
+  addressTransactions,
+  detail,
+  balance,
+  isLoading,
+}: InferGetServerSidePropsType<typeof getServerSideProps>) {
   const [isQrCodeClicked, setIsQrCodeClicked] = useState(false);
   const router = useRouter();
   const cid = router.query.cid?.toString()!;
@@ -41,15 +64,19 @@ export default function VerifiedContract({ data }) {
             <VerifiedGreenTickIcon size={18} />
           </div>
           <ContractSegmentOne
-            creator={data.verifiedContractData.creatorAddress}
-            balance={data.verifiedContractData.balance}
+            creator={detail.creator_address_hash}
+            balance={{ value: balance, symbol: DMX_TOKEN_SYMBOL }}
             setIsQrCodeClicked={setIsQrCodeClicked}
           />
         </div>
       </GradientCardContainer>
       <GradientCardContainer className="relative mt-6">
         <div className="md:p-10 px-5 py-10">
-          <ContractSegmentTwo data={data} />
+          <ContractSegmentTwo
+            addressHash={cid}
+            isLoading={isLoading}
+            transactions={addressTransactions}
+          />
         </div>
       </GradientCardContainer>
       {isQrCodeClicked && (
@@ -70,7 +97,7 @@ function ContractSegmentOne({
   setIsQrCodeClicked,
 }: {
   creator: string;
-  balance: { value: number; symbol: string };
+  balance: { value: string; symbol: string };
   setIsQrCodeClicked: Dispatch<SetStateAction<boolean>>;
 }): JSX.Element {
   const [isContractAddressCopied, setIsContractAddressCopied] = useState(false);
@@ -135,7 +162,7 @@ function ContractSegmentOne({
             value={balance.value}
             decimalScale={8}
             suffix={` ${balance.symbol}`}
-            data-testid="contract-wallet-balance-value"
+            data-testid="contract-address-balance-value"
           />
         </div>
       </div>
@@ -143,19 +170,32 @@ function ContractSegmentOne({
   );
 }
 
-function ContractSegmentTwo({ data }) {
-  const [selectedTab, setSelectedTab] = useState(ContractTabsTitle.Code);
+function ContractSegmentTwo({
+  addressHash,
+  isLoading,
+  transactions,
+}: {
+  addressHash: string;
+  isLoading?: boolean;
+  transactions: AddressTransactionsProps;
+}) {
+  const [selectedTab, setSelectedTab] = useState(
+    ContractTabsTitle.Transactions
+  );
 
   return (
     <div>
       <ContractTabs selectedTab={selectedTab} setSelectedTab={setSelectedTab} />
       {selectedTab === ContractTabsTitle.Transactions && (
-        <ContractTransactionsList
-          contractTransactions={data.transactions}
-          contractTransactionPages={data.pages}
+        <TransactionDetails
+          data={transactions}
+          pathname={`/contract/${addressHash}`}
+          type="address"
+          isLoading={isLoading}
+          isHeaderDisplayed={false}
         />
       )}
-      {selectedTab === ContractTabsTitle.Tokens && (
+      {/* {selectedTab === ContractTabsTitle.Tokens && (
         <ContractTokensList
           contractTokenList={data.tokens}
           contractTokenListPage={data.tokenPages}
@@ -163,8 +203,8 @@ function ContractSegmentTwo({ data }) {
           otherTokens={data.verifiedContractData.otherTokens}
           networth={data.verifiedContractData.networth}
         />
-      )}
-      {selectedTab === ContractTabsTitle.Code && (
+      )} */}
+      {/* {selectedTab === ContractTabsTitle.Code && (
         <ContractCode
           contractName={data.verifiedContractData.contractName}
           compilerVersion={data.verifiedContractData.compilerVersion}
@@ -176,7 +216,7 @@ function ContractSegmentTwo({ data }) {
           pages={data.readContractPages}
           writeContractData={data.verifiedContractData.writeContractData}
         />
-      )}
+      )} */}
     </div>
   );
 }
@@ -201,18 +241,56 @@ const fixedTitle = {
   copied: "Copied!",
 };
 
-export async function getServerSideProps() {
-  // Fetch data from external API
-  const data = {
-    verifiedContractData,
-    pages,
-    transactions,
-    tokenPages,
-    tokens,
-    readContractPages,
-  };
-  // Pass data to the page via props
-  return { props: { data } };
+export async function getServerSideProps(
+  context: GetServerSidePropsContext
+): Promise<GetServerSidePropsResult<ContractDetailProps>> {
+  const { network, ...params } = context.query;
+  const cid = params?.cid?.toString().trim() as string;
+
+  console.log({ cid });
+  if (!isAlphanumeric(cid)) {
+    return { notFound: true };
+  }
+
+  try {
+    const contractDetail = await WalletAddressApi.getDetail(
+      network as NetworkConnection,
+      cid
+    );
+
+    const hasInvalidParams =
+      !isNumeric(params?.block_number as string) ||
+      !isNumeric(params?.items_count as string) ||
+      !isNumeric(params?.page_number as string) ||
+      !isNumeric(params?.index as string);
+
+    const addressTransactions = hasInvalidParams
+      ? await WalletAddressApi.getAddressTransactions(
+          network as NetworkConnection,
+          cid
+        )
+      : await WalletAddressApi.getAddressTransactions(
+          network as NetworkConnection,
+          cid,
+          params?.block_number as string,
+          params?.items_count as string,
+          params?.index as string
+        );
+
+    return {
+      props: {
+        balance: utils.formatEther(contractDetail.coin_balance ?? "0"),
+        detail: contractDetail,
+        addressTransactions: {
+          transactions: addressTransactions.items,
+          nextPageParams:
+            addressTransactions.next_page_params as TxnNextPageParamsProps,
+        },
+      },
+    };
+  } catch (e) {
+    return { notFound: true };
+  }
 }
 
 const onCopyAddressIconClick = async (

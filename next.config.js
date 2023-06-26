0d4a8594eb32fcbc9f760ba46f26d189830450d6
Harsh R <53080940+fullstackninja864@users.noreply.github.com>
2023-06-26 09:47:33 +0530

    Update next.config.js
    Co-authored-by: Lyka Labrada <lykalabrada@gmail.com>

--- next.config.js
+++ next.config.js
@@ -15,7 +15,7 @@ const securityHeaders = [
       };` +
       `style-src 'self' fonts.googleapis.com 'unsafe-inline';` +
       `font-src fonts.gstatic.com;` +
-      `connect-src 'self' blockscout-index.changi.dfi.team solc-bin.ethereum.org/linux-amd64/list.json raw.githubusercontent.com/blockscout/solc-bin/main/vyper.list.json blockscout.com 35.197.159.154:4000 ${
+      `connect-src 'self' blockscout-index.changi.dfi.team solc-bin.ethereum.org/linux-amd64/list.json raw.githubusercontent.com/blockscout/solc-bin/main/vyper.list.json blockscout.com ${
         process.env.NODE_ENV === "development"
           ? `ws://localhost:3000/_next/webpack-hmr base-goerli.blockscout.com eth-goerli.blockscout.com 34.87.158.111:4000`
           : ""

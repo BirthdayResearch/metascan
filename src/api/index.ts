37b2cfb14b5fef9558fb9463992c4d5c8fae45d3
Pierre Gee <pierre.gabaldon@gmail.com>
2023-09-06 16:29:58 +0800

    chore(ops): update default (#335)
    * fix(ui-ux): modified network from testnet to changi
    
    * chore(package): update eslint to 6.6.0
    
    * update network
    
    * fix
    
    * chore(ops): update config
    
    * chore(ops): update default
    
    * chore(ops): update default
    
    ---------
    
    Co-authored-by: chloe <44501120+chloezxyy@users.noreply.github.com>

--- src/api/index.ts
+++ src/api/index.ts
@@ -22,14 +22,14 @@ export const V1_TRANSACTION_URL =
   "api?module=transaction&action=gettxinfo&txhash=";
 
 export const getBaseUrl = (network: NetworkConnection) => {
-  if (network === NetworkConnection.Changi) {
-    return process.env.NEXT_PUBLIC_RPC_URL_CHANGI;
+  if (network === NetworkConnection.MainNet) {
+    return process.env.NEXT_PUBLIC_RPC_URL_MAINNET;
   }
   if (network === NetworkConnection.TestNet) {
     return process.env.NEXT_PUBLIC_RPC_URL_TESTNET;
   }
 
-  return process.env.NEXT_PUBLIC_RPC_URL_MAINNET;
+  return process.env.NEXT_PUBLIC_RPC_URL_CHANGI;
 };
 
 export function filterParams(params: { key: string; value }[]): string {

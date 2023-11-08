d0d75e6d61968356e91a7b18e1ae6f97f014311e
pierregee <pierre@cakedefi.com>
2023-11-08 13:29:58 +0800

    fix ci

--- apps/server/src/main.ts
+++ apps/server/src/main.ts
@@ -5,6 +5,10 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const PORT = process.env.PORT || 3001;
-  app.listen(PORT).then(() => console.log(`Started server on port ${PORT}`));
+  // eslint-disable-next-line @typescript-eslint/no-floating-promises
+  app.listen(PORT).then(() => {
+    // eslint-disable-next-line no-console 
+    console.log(`Started server on port ${PORT}`)
+  });
 }
 void bootstrap();

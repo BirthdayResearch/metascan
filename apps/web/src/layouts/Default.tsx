b50c1c5064cb7ec4e94464f6482f83cb54834406
nattadex <elocinnat99@gmail.com>
2023-11-09 14:23:32 +0800

    removed unused code

--- apps/web/src/layouts/Default.tsx
+++ apps/web/src/layouts/Default.tsx
@@ -11,7 +11,6 @@ import { IconGradient } from "@components/icons/IconGradient";
 import Footer from "@components/Footer";
 import Header from "./components/Header";
 import MainContainer from "./components/MainContainer";
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
 const title = "MetaScan";
 export const appName = "meta.defiscan.live";
@@ -44,12 +43,6 @@ export function Default({ children }: PropsWithChildren): JSX.Element {
   useEffect(() => {
     setMounted(true);
   }, []);
-  const [queryClient] = useState(
-    () =>
-      new QueryClient({
-        defaultOptions: { queries: { refetchOnWindowFocus: false } },
-      }),
-  );
 
   return (
     <div className="flex flex-col min-h-screen">
@@ -99,26 +92,24 @@ export function Default({ children }: PropsWithChildren): JSX.Element {
           <ConnectKitProvider mode="dark" options={{ initialChainId: 0 }}>
             <StoreProvider>
               <NetworkProvider>
-                <QueryClientProvider client={queryClient}>
-                  <Header />
-                  <IconGradient />
-                  <MainContainer>{children}</MainContainer>
+                <Header />
+                <IconGradient />
+                <MainContainer>{children}</MainContainer>
+                <div
+                  data-testid="bg-purple-gradient"
+                  className="w-full h-screen absolute z-[-1] bg-no-repeat top-0 left-0 lg:bg-[url('/background/gradient-purple.png')] md:bg-[url('/background/gradient-purple-tablet.png')] bg-[url('/background/gradient-purple-mobile.png')]"
+                />
+                <div
+                  data-testid="bg-blue-gradient"
+                  className="w-full h-screen absolute z-[-1] mix-blend-screen bg-no-repeat bg-contain bg-right lg:bg-[url('/background/gradient-blue)] md:bg-[url('/background/gradient-blue-tablet.png')] bg-[url('/background/gradient-blue-mobile.png')]"
+                />
+                <div className="relative w-full h-full">
+                  <Footer />
                   <div
-                    data-testid="bg-purple-gradient"
-                    className="w-full h-screen absolute z-[-1] bg-no-repeat top-0 left-0 lg:bg-[url('/background/gradient-purple.png')] md:bg-[url('/background/gradient-purple-tablet.png')] bg-[url('/background/gradient-purple-mobile.png')]"
+                    data-testid="bg-footer-image"
+                    className="fill w-full h-screen absolute z-[-2] mix-blend-screen bottom-0 left-0 bg-no-repeat bg-cover bg-bottom lg:bg-[url('/background/footer.png')]  md:bg-[url('/background/footer-tablet.png')] bg-[url('/background/footer-mobile.png')]"
                   />
-                  <div
-                    data-testid="bg-blue-gradient"
-                    className="w-full h-screen absolute z-[-1] mix-blend-screen bg-no-repeat bg-contain bg-right lg:bg-[url('/background/gradient-blue)] md:bg-[url('/background/gradient-blue-tablet.png')] bg-[url('/background/gradient-blue-mobile.png')]"
-                  />
-                  <div className="relative w-full h-full">
-                    <Footer />
-                    <div
-                      data-testid="bg-footer-image"
-                      className="fill w-full h-screen absolute z-[-2] mix-blend-screen bottom-0 left-0 bg-no-repeat bg-cover bg-bottom lg:bg-[url('/background/footer.png')]  md:bg-[url('/background/footer-tablet.png')] bg-[url('/background/footer-mobile.png')]"
-                    />
-                  </div>
-                </QueryClientProvider>
+                </div>
               </NetworkProvider>
             </StoreProvider>
           </ConnectKitProvider>

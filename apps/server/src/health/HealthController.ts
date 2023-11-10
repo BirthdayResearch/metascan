16711a15169b588d9a1e500ba6ce5887a4ad1929
Harsh R <53080940+fullstackninja864@users.noreply.github.com>
2023-11-10 07:42:13 +0530

    feature(serve): added health endpoint (#350)
    * feature(serve): added health endpoint
    
    * lint fix

---
+++ apps/server/src/health/HealthController.ts
@@ -0,0 +1,15 @@
+import { Controller, Get } from '@nestjs/common';
+import { HealthCheck, HealthCheckService } from '@nestjs/terminus';
+import { SkipThrottle } from '@nestjs/throttler';
+
+@Controller('health')
+export class HealthController {
+  constructor(private readonly health: HealthCheckService) {}
+
+  @SkipThrottle()
+  @Get()
+  @HealthCheck()
+  check() {
+    return this.health.check([]);
+  }
+}

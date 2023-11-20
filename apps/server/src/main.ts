4f4832d779b05270740a9f99ee996b6c38ac6ec8
chloe <44501120+chloezxyy@users.noreply.github.com>
2023-11-20 17:43:09 +0800

    fix: use MetascanServerApp

--- apps/server/src/main.ts
+++ apps/server/src/main.ts
@@ -1,14 +1,9 @@
-import { NestFactory } from '@nestjs/core';
-
 import { AppModule } from './app.module';
+import { MetascanServerApp } from './MetascanServerApp';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  const PORT = process.env.PORT || 5741;
-  // eslint-disable-next-line @typescript-eslint/no-floating-promises
-  app.listen(PORT).then(() => {
-    // eslint-disable-next-line no-console
-    console.log(`Started server on port ${PORT}`);
-  });
+  const app = new MetascanServerApp(AppModule);
+  await app.start();
 }
+
 void bootstrap();

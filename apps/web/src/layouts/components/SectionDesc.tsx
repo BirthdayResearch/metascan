62e020496278659f292061a8c2834e2bbee6ed61
nattadex <elocinnat99@gmail.com>
2023-11-22 16:42:36 +0800

    revert unnecessayr change for sectionDesc

--- apps/web/src/layouts/components/SectionDesc.tsx
+++ apps/web/src/layouts/components/SectionDesc.tsx
@@ -1,18 +1,15 @@
 import clsx from "clsx";
-import Link from "@components/commons/Link";
 
-export default function SectionDesc({ title, href }: { title: string; href?: string }): JSX.Element {
+export default function SectionDesc({ title }: { title: string }): JSX.Element {
   return (
     <div className="flex flex-col items-center text-center my-4 mx-1 md:mx-[148px] lg:mx-0">
-      <Link
+      <span
         className={clsx(
           "font-semibold text-white-50 text-sm leading-10 -tracking-[0.01em]",
         )}
-        href={href}
-        target="_blank"
       >
         {title}
-      </Link>
+      </span>
     </div>
   );
 }
